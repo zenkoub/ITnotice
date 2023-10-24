c2cb1a0723baae96627a215a624d73ed13c61a9e
zen <Windows10Pro@macz.local>
2023-10-24 21:37:41 +0700

    init

---
+++ pscp-client/src/app/pscp/page.js
@@ -0,0 +1,16 @@
+import Navbar from "@/components/Navbar";
+
+export default function Pscp() {
+    const key = [
+        { title: "เว็บ" },
+        { title: "ไซต์" },
+        { title: "ITnotice" }
+    ]
+    return (
+        <>
+            <div className="">
+                <Navbar data={hee} />
+            </div>
+        </>
+    )
+}

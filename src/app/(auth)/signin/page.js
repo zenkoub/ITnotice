91d1c44548f0491cdf1933b2c342b6bb32c6c471
Zensos <rockemaner@gmail.com>
2023-11-07 23:41:08 +0700

    update: onclick and filter service

--- src/app/(auth)/signin/page.js
+++ src/app/(auth)/signin/page.js
@@ -1,15 +1,25 @@
-import Navbar from "@/components/Navbar";
+"use client"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function signin() {
-    const key = [
-        { title: "เว็บ" },
-        { title: "ไซต์" },
-        { title: "ITnotice" }
-    ]
+    const [repos, setRepos] = useState([]);
+
+    useEffect(() => {
+      // Make an Axios request to the GitHub API on the client side
+      axios.get('https://api.github.com/users/Zensos/repos')
+        .then((response) => {
+          setRepos(response.data);
+        })
+        .catch((error) => {
+          console.error('Error fetching GitHub repositories:', error);
+        });
+    }, []);
+    console.log(repos)
     return (
         <>
             <div className="text-white">
-                HEE
+                
             </div>
         </>
     )

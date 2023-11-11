dddf3177bfabe7807a4fe11a963e2cd510d27cf8
WIN <66070065@kmitl.ac.th>
2023-11-12 00:11:34 +0700

    update: all path‼️

--- src/app/(directory)/fe/page.js
+++ src/app/(directory)/fe/page.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Loader from '@/components/Loader'
 
 export default async function page() {
-    const result = await (await axios('https://23b3-110-164-110-135.ngrok-free.app/tasks/PSCP')).data
+    const result = await (await axios('https://23b3-110-164-110-135.ngrok-free.app/tasks/FE')).data
 
     return (
         <>
